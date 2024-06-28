dc10ea07a2ce971ddc6be884881f45255fa36521
Kaung Zaw Hein <kaungzawhein1963@gmail.com>
2024-06-28 23:14:42 +0630

    api update

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,7 +1,7 @@
 export const exerciseOptions = {
     method: 'GET',
     headers: {
-      'X-RapidAPI-Key': "9649619860msh5df97cb7475fb51p144047jsn631f25f59e48",
+      'X-RapidAPI-Key': "f3b9d18db7msh8955b2c2a4dd921p174dafjsn4c0d9396da90",
       'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
     },
   };
